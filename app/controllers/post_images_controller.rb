090ad6d86ef68f557f6f1f6f9cf85fa0fd913d58
EC2 Default User <126648712+taka0705@users.noreply.github.com>
2023-03-10 08:55:59 +0000

    3-21まで

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,9 +6,12 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
-  end
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
+  end  
 
   def index
     @post_images=PostImage.all
@@ -16,6 +19,7 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image=PostImage.find(params[:id])
+    @post_comment=PostComment.new
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
   # get 'post_images/new'
   # get 'post_images/index'
   # get 'post_images/show'
-  resources :users,only: [:show, :edit, :update]
-  resources :post_images,only: [:new, :create, :index, :show, :destroy]
   devise_for :users
   root to: "homes#top"
   get "homes/about",as: "about"
+  resources :post_images,only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites,only:[:create, :destroy]
+    resources :post_comments,only: [:create, :destroy]
+  end
+  resources :users,only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
